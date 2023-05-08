f8266754105466b83f49118a140e4f25d448cd48
LVL <luis.vinelerma@telefonica.com>
2023-05-08 15:21:06 +0200

    Generated serial version UID

--- src/main/java/com/mockitotutorial/happyhotel/booking/BusinessException.java
+++ src/main/java/com/mockitotutorial/happyhotel/booking/BusinessException.java
@@ -2,4 +2,9 @@ package com.mockitotutorial.happyhotel.booking;
 
 public class BusinessException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3352679405221044346L;
+
 }
